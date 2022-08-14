afa3eafae80577f8f9f3c9f22fc554f0267a0d8b
Constipatus <jd_quesada@hotmail.com>
2022-08-14 02:38:21 -0700

    Create buttons for rainbow colored sketch and revert to black

--- java.js
+++ java.js
@@ -1,21 +1,44 @@
 
-//Creates black sketch markings on the grid
-const sketch = document.querySelector('.sketchGrid')
-sketch.addEventListener('mouseover', function (e) {
-    e.target.style.backgroundColor = 'black';
+//Creates base black sketch markings on the grid
+const sketch = document.querySelector('.sketchGrid');
+sketch.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = 'Black';
+});
+
+//Button to override black to randomized colros and back
+const rainbowSketch = document.querySelector('#randomColor');
+
+
+rainbowSketch.addEventListener('click', () => {
+    sketch.removeEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'Black';
+    })
+    sketch.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = "#" + (Math.floor(Math.random()*16777215).toString(16));
+    });
+});
+
+const blackSketch = document.querySelector('#blackColor');
+blackSketch.addEventListener('click', () => {
+    sketch.removeEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = "#" + (Math.floor(Math.random()*16777215).toString(16));
+    })
+    sketch.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'Black';
+    })
 })
+
 
 //Base settings to create grid divs
 newGrid = document.createElement('div');
 grid = document.querySelector('.sketchGrid');
 
 //Base grid size
-a = 30
-
+let a = 30
 
 //Create grid with input scales
 function gridSize () {
-    if (a>100||a<6){
+    if (a>100||a<10){
         return
     };
     for (i=0; i<(a*a); i++) {
@@ -29,18 +52,20 @@ function gridSize () {
 window.addEventListener('load',gridSize)
 
 
-//Function and parameters to set the new grid
+//Function and parameters prompt for a new grid
 const newSize = document.getElementById('newSize');
 newSize.addEventListener('click', () => {
     a = prompt ("Pick a size from 10-100");
-    newGrid ();
-})
 
-function newGrid() {
     while (sketch.firstChild) {
         sketch.removeChild(sketch.firstChild)
     };
+
     sketch.style.cssText = `grid-template-columns: repeat(${a}, 1fr); 
         grid-template-rows: repeat(${a}, 1fr);`;
     gridSize ();
-}
+})
+
+
+//Function to remove previous grid and create another based on player entry
+//function newGrid () {}
